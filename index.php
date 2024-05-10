2081fcf10bad9068930ca1b483020f9eb786d784
ParshaNugraha <pramudyashaka27@gmail.com>
2024-05-11 01:18:22 +0700

    perbaikan link

--- index.php
+++ index.php
@@ -99,7 +99,7 @@
                     <div class="row justify-content-center">
                         <!-- Earnings (Monthly) Card Example -->
                         <div class="col-xl-3 col-md-6 mb-4">
-                            <a href="analisis.php" style="text-decoration: none;">
+                            <a href="charts.php" style="text-decoration: none;">
                                 <div class="card border-left-primary shadow h-100 py-2">
                                     <div class="card-body">
                                         <div class="row no-gutters align-items-center">
@@ -118,7 +118,7 @@
 
                         <!-- Earnings (Monthly) Card Example -->
                         <div class="col-xl-3 col-md-6 mb-4">
-                            <a href="pendapatan.php" style="text-decoration: none;">
+                            <a href="tables.php" style="text-decoration: none;">
                                 <div class="card border-left-success shadow h-100 py-2">
                                     <div class="card-body">
                                         <div class="row no-gutters align-items-center">
